f35ca155bb77c73dbd95201153a06d78f10d94bd
Guilherme Mendes <mendes.silva.guilherme@gmail.com>
2021-05-13 02:05:40 -0300

    Initial Commit

---
+++ Studio Ghibli Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Studio Ghibli Movies
+//
+//  Created by Guilherme Mendes on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
