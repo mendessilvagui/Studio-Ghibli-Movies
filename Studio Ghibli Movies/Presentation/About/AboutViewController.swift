94cb30d63219242d19feaf6cd5d41c071778c7a4
Guilherme Mendes <mendes.silva.guilherme@gmail.com>
2021-10-15 09:21:58 -0300

    Added About page with app version, e-mail to contact and app creator

--- Studio Ghibli Movies/Presentation/About/AboutViewController.swift
+++ Studio Ghibli Movies/Presentation/About/AboutViewController.swift
@@ -9,9 +9,65 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var contactView: UIView!
+    @IBOutlet weak var contactLabel: UILabel!
+    @IBOutlet weak var versionView: UIView!
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var logoView: UIView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var createdByLabel: UILabel!
+
+    //MARK: - UIViewController lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        styleViews()
+        setupViews()
+        fetchVersion()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.navigationItem.title = "About"
+    }
+
+    // MARK: - Private methods
+
+    private func styleViews() {
+        Style.styleViewBackground(imageView: imageView)
+
+        contactView.layer.cornerRadius = 10
+        contactView.addShadowToView(color: UIColor.black.cgColor, radius: 5, offset: .zero, opacity: 0.5)
+        versionView.layer.cornerRadius = 10
+        versionView.addShadowToView(color: UIColor.black.cgColor, radius: 5, offset: .zero, opacity: 0.5)
+        logoView.layer.cornerRadius = 10
+        logoView.addShadowToView(color: UIColor.black.cgColor, radius: 5, offset: .zero, opacity: 0.5)
+        logoImageView.layer.cornerRadius = 10
+
+        createdByLabel.addShadowToView(color: UIColor.black.cgColor, radius: 3, offset: CGSize(width: 2, height: 2), opacity: 0.75)
+        createdByLabel.layer.masksToBounds = false
+    }
+
+    private func setupViews() {
+        let contactsTouchRecognizer = UITapGestureRecognizer(target: self, action: #selector(onEmailTouched))
+        contactView.addGestureRecognizer(contactsTouchRecognizer)
+    }
+
+    private func fetchVersion() {
+        if let version = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String,
+            let buildNumber = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as? String {
+            versionLabel.text = "\(version) (\(buildNumber))"
+        } else {
+            versionLabel.text = "???"
+        }
+    }
+
+    // MARK: - Touch gestures
+
+    @objc func onEmailTouched() {
+        if let url = URL(string: "mailto:guimendes93@gmail.com") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
 }
--- Studio Ghibli Movies/Presentation/Menu/MenuViewController.swift
+++ Studio Ghibli Movies/Presentation/Menu/MenuViewController.swift
@@ -12,7 +12,10 @@ class MenuViewController: UITabBarController {
     private var moviesViewController = MoviesViewController()
     private var favoriteMoviesViewController = FavoriteMoviesViewController()
     private var profileViewController = ProfileViewController()
+    private var aboutViewContoller = AboutViewController()
     private var allViewControllers = [UIViewController]()
+
+    //MARK: - UIViewController lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,8 +26,10 @@ class MenuViewController: UITabBarController {
         setUpTabBarItems()
     }
 
+    //MARK: - SetUp methods
+
     private func setViewControllers() {
-        setViewControllers([moviesViewController, favoriteMoviesViewController, profileViewController], animated: true)
+        setViewControllers([moviesViewController, favoriteMoviesViewController, profileViewController, aboutViewContoller], animated: true)
     }
 
     private func setUpNavBar() {
@@ -47,7 +52,7 @@ class MenuViewController: UITabBarController {
     }
 
     private func setUpTabBarItems() {
-        allViewControllers = [moviesViewController, favoriteMoviesViewController, profileViewController]
+        allViewControllers = [moviesViewController, favoriteMoviesViewController, profileViewController, aboutViewContoller]
 
         for viewController in allViewControllers {
 
@@ -63,6 +68,10 @@ class MenuViewController: UITabBarController {
                 self.navigationItem.title = "Profile"
                 viewController.title = "Profile"
                 viewController.tabBarItem.image = UIImage(systemName: "person")
+            case aboutViewContoller:
+                self.navigationItem.title = "About"
+                viewController.title = "About"
+                viewController.tabBarItem.image = UIImage(systemName: "info.circle")
             default:
                 break
             }
